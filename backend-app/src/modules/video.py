517ba0ab8d7efc05b7fd5f4dc3b7ef5f2329048d
jmv74211 <jmv74211@gmail.com>
2019-07-10 21:22:53 +0200

    Add a new features to video module #2

--- backend-app/src/modules/video.py
+++ backend-app/src/modules/video.py
@@ -1,31 +1,40 @@
-from picamera import PiCamera, Color
-from time import sleep
+from picamera import PiCamera
+from picamera import Color
+import subprocess
 import time
-import os
+import datetime as dt
 
 """
     Class to administer the video resource
 """
 
 class Video:
+    """ Constructor
 
-##############################################################################################
-
+    Parameters:
+        file_path (str): Path where video photo files.
+        showDatetime (bool): Activate/deactivate datetime info in the video
+        resolution (str): Resolution level. [HIGH, MEDIUM, LOW]. (default is HIGH)
+        hflip (bool): Horizontal flip. (default is False)
+        vflip (bool): Vertical flip. (default is False)
     """
-        Constructor class
 
-        Param file_path: Path where save video files.
-    """
-
-    def __init__(self,file_path = "/home/jmv74211/Escritorio/"):
+    def __init__(self, file_path="/home/jmv74211/Escritorio/", showDatetime=False,
+                 resolution="HIGH", hflip=False, vflip=False):
         self.file_path = file_path
         self.camera = PiCamera()
+        self.showDatetime = showDatetime
+        self.resolution = resolution
+        self.set_resolution(resolution)
+        self.hflip = hflip
+        self.vflip = vflip
 
-##############################################################################################
-
+    ##############################################################################################
 
     """
-        Returns a video name with the current date
+    
+    Returns a video name with the current date
+    
     """
 
     def get_file_name(self):
@@ -34,72 +43,157 @@ class Video:
         day = time.strftime("%m")
 
         hour = time.strftime("%H")
-        minute= time.strftime("%M")
+        minute = time.strftime("%M")
         second = time.strftime("%S")
 
-        file_name = "VID_{}{}{}_{}{}{}.h264".format(year,month,day,hour,minute,second)
+        file_name = "VID_{}{}{}_{}{}{}.h264".format(year, month, day, hour, minute, second)
 
         return file_name
 
-##############################################################################################
+    ##############################################################################################
+
+    """ It allows to modify the video resolution.
+
+    Parameters:
+        resolution (str): Resolution level. [LOW, MEDIUM, HIGH] (default is HIGHT) .
 
     """
-        It allows to modify the video resolution or add a text inside it.
-        Will be improved in next versions.
 
-        Param resolution: Resolution level. Default best.
-        Param annotate_text: True for add text to the video. Default false
+    def set_resolution(self, resolution="HIGH"):
+        # Resolution info: https://picamera.readthedocs.io/en/release-1.12/fov.html
 
+        # MAX video resolution: (1920, 1080) and MAX Framerate 30
+
+        if resolution == "HIGH":
+            self.camera.resolution = (1920, 1080)
+            self.camera.framerate = 30
+            self.resolution = "HIGH"
+        elif resolution == "MEDIUM":
+            self.camera.resolution = (1280, 720)
+            self.camera.framerate = 40
+            self.resolution = "MEDIUM"
+        elif resolution == "LOW":
+            self.camera.resolution = (640, 480)
+            self.camera.framerate = 60
+            self.resolution = "LOW"
+
+    ##############################################################################################
+
+    """ It allows to rotate the video file.
+
+    Parameters:
+        degrees (int): Number of degrees to rotate
     """
 
-    def set_configuration(self,resolution="best",annotate_text=False):
-        if resolution == "best":
-            # Ajustar a resolución máxima: Fotos 2592x1944, Vídeos 1920x1080
-            self.camera.resolution = (1920,1080)
-            self.camera.framerate = 15
+    def rotate(self, degrees):
+        self.camera.rotation = degrees % 360
+
+    ##############################################################################################
+
+    """ It allows activate/deactivate vertical flip.
+
+    Parameters:
+        status (bool): Active or deactivated status. (default is False).
+    """
+
+    def set_vflip(self, status=False):
+        self.vflip = status
+
+        if status:
+            self.camera.vflip = True
+
+    ##############################################################################################
+
+    """ It allows activate/deactivate horizontal flip.
+
+   Parameters:
+       status (bool): Active or deactivated status. (default is False).
+    """
+
+    def set_hflip(self, status=False):
+        self.hflip = status
+
+        if status:
+            self.camera.hflip = True
+
+    ##############################################################################################
+
+    """ It allows to capture a video and save the file. Then, it is converted to mp4 format.
+
+    Parameters:
+        record_time (int): Record time. (default is 10).
+    """
+
+    def record_video(self, record_time=10):
+
+        video_name = self.file_path + "/" + self.get_file_name()
+
+        if self.showDatetime:
+            self.camera.annotate_background = Color('black')
+            self.camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+            self.camera.start_recording(video_name)
+            print("The recording has started")
+            start = dt.datetime.now()
+            while (dt.datetime.now() - start).seconds < record_time:
+                self.camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                self.camera.wait_recording(0.2)
+            self.camera.stop_recording()
+            print("The recording has finished")
+
         else:
-            self.camera.resolution = (1280,720)
-            self.camera.framerate = 15
+            self.camera.start_preview()
+            self.camera.start_recording(video_name)
+            print("The recording has started")
+            self.camera.wait_recording(record_time)
+            self.camera.stop_recording()
+            self.camera.stop_preview()
+            print("The recording has finished")
 
-        if annotate_text:
-            self.camera.annotate_text = get_file_name()
-            self.camera.annotate_text_size = 50
-            self.camera.annotate_background = Color('blue')
-            self.camera.annotate_foreground = Color('yellow')
+        # Convert from .h264 to .mp4
+        self.convert_video_to_mp4(video_name)
 
-##############################################################################################
+    ##############################################################################################
 
+    """ It allows to convert from.h264 to mp4 video format.
+
+    Parameters:
+        video_path (str): Name of .h264 video path to convert.
     """
-        It allows to capture a video and save the file.
 
-        Param time: Record time. Default is 10.
+    def convert_video_to_mp4(self, video_path):
+
+        video_name_converted = video_path[:-4]  # Deletes h264
+        video_name_converted += "mp4"  # Adds new format
+
+        # Make a subprocess. Father process waits until it finishes
+        subprocess.call(['MP4Box', '-add', video_path, video_name_converted])
+
+        # Call delete video function
+        self.delete_video(video_path)
+        print("Video has been converted to mp4")
+
+    ##############################################################################################
+
+    """ It allows to delete de video specified. It is usually called after converting a video.
+    
+    Parameters:
+        video_path (str): Video name path to delete
+        
     """
-    # Version 1. Problem detected: Only record 60% of total time
-    #def record_video(self,time=10):
-        #self.camera.start_preview()
-        #self.camera.start_recording(self.file_path+ "/" +self.get_file_name())
-        #print("Se ha empezado a grabar")
-        #self.camera.wait_recording(time)
-        #self.camera.stop_recording()
-        #self.camera.stop_preview()
-        #self.camera.close()
-        #print("La grabación ha finalizado")
 
-    def record_video(self,record_time=10):
-        sentence = "raspivid -t " + repr(record_time*1000)  +" -o " + self.file_path+ "/" +self.get_file_name()
-        process = os.popen(sentence)
-        print("Se ha empezado a grabar")
-        self.camera.close()
-        time.sleep(record_time)
-        print("La grabación ha finalizado")
+    def delete_video(self, video_path):
 
-##############################################################################################
+        # Make a subprocess for removing. Father process waits until it finishes
+        subprocess.call(['rm', video_path])
 
-    """
-        It allows to liberate a camera resource.
+    ##############################################################################################
+
+    """ It allows to liberate a camera resource.
+    
     """
 
     def close(self):
         self.camera.close()
 
-##############################################################################################
+    ##############################################################################################
