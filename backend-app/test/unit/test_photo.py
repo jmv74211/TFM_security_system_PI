e2ac0fbe6715d91c8b32245f4d8da99bd6677bdf
jmv74211 <jmv74211@gmail.com>
2019-07-11 13:11:32 +0200

    Making some changes in Photo unit test #3

--- backend-app/test/unit/test_photo.py
+++ backend-app/test/unit/test_photo.py
@@ -43,7 +43,10 @@ class TestPhotoModule(unittest.TestCase):
         # Template name
         file_name = "IMG_{}{}{}_{}{}{}.jpg".format(year, month, day, hour, minute, second)
 
-        # Check
+        # ----------------------------------------------------------------------------------------#
+        #                                     CHECKS                                              #
+        # ----------------------------------------------------------------------------------------#
+
         self.assertEqual(file_name, self.camera_photo.get_file_name())
 
     ##############################################################################################
@@ -62,16 +65,18 @@ class TestPhotoModule(unittest.TestCase):
         path = "/home/random"
         self.camera_photo = Photo(path)
 
-        # Checks
+        # ----------------------------------------------------------------------------------------#
+        #                                     CHECKS                                              #
+        # ----------------------------------------------------------------------------------------#
+
         self.assertEqual(current_file_path, self.file_path)
-
         self.assertEqual(new_file_path, modified_file_path)
-
         self.assertEqual(path, self.camera_photo.file_path)
 
     ##############################################################################################
 
     def test3_set_resolution(self):
+        # Change resolution values
         current_default_resolution = self.camera_photo.resolution
         current_default_resolution_value = self.camera_photo.camera.resolution
         current_default_framerate = self.camera_photo.camera.framerate
@@ -91,7 +96,15 @@ class TestPhotoModule(unittest.TestCase):
         current_low_resolution_value = self.camera_photo.camera.resolution
         current_low_framerate = self.camera_photo.camera.framerate
 
-        # Checks
+        self.camera_photo.set_resolution("adasdadasdas")
+        current_error_resolution = self.camera_photo.resolution
+        current_error_resolution_value = self.camera_photo.camera.resolution
+        current_error_framerate = self.camera_photo.camera.framerate
+
+        # ----------------------------------------------------------------------------------------#
+        #                                     CHECKS                                              #
+        # ----------------------------------------------------------------------------------------#
+
         self.assertEqual(current_default_resolution, "HIGH")
         self.assertEqual(current_default_resolution_value, (1920, 1080))
         self.assertEqual(current_default_framerate, 30)
@@ -107,6 +120,10 @@ class TestPhotoModule(unittest.TestCase):
         self.assertEqual(current_low_resolution, "LOW")
         self.assertEqual(current_low_resolution_value, (640, 480))
         self.assertEqual(current_low_framerate, 60)
+
+        self.assertEqual(current_error_resolution, "LOW")  # if it is a wrong resolution, value will not
+        self.assertEqual(current_error_resolution_value, (640, 480))  # change, so it takes the last one.
+        self.assertEqual(current_error_framerate, 60)
 
     ##############################################################################################
 
@@ -135,14 +152,28 @@ class TestPhotoModule(unittest.TestCase):
         self.camera_photo.rotate(270)
         state_6 = self.camera_photo.camera.rotation
 
-        # Checks
+        # Test with a negative value
+        self.camera_photo.rotate(-30)
+        state_7 = self.camera_photo.camera.rotation
+        self.camera_photo.rotate(-90)
+        state_8 = self.camera_photo.camera.rotation
+        self.camera_photo.rotate(-270)
+        state_9 = self.camera_photo.camera.rotation
+
+        # ----------------------------------------------------------------------------------------#
+        #                                     CHECKS                                              #
+        # ----------------------------------------------------------------------------------------#
+
         self.assertEqual(current_degrees, 0)
         self.assertEqual(state_1, 90)
         self.assertEqual(state_2, 90)
         self.assertEqual(state_3, 180)
-        self.assertEqual(state_4, 90)
+        self.assertEqual(state_4, 180)
         self.assertEqual(state_5, 180)
         self.assertEqual(state_6, 270)
+        self.assertEqual(state_7, 270)
+        self.assertEqual(state_8, 270)
+        self.assertEqual(state_9, 90)
 
     ##############################################################################################
 
@@ -154,6 +185,10 @@ class TestPhotoModule(unittest.TestCase):
 
         self.camera_photo.set_vflip(False)
         deactivated_value = self.camera_photo.vflip
+
+        # ----------------------------------------------------------------------------------------#
+        #                                     CHECKS                                              #
+        # ----------------------------------------------------------------------------------------#
 
         self.assertFalse(default_value)
         self.assertTrue(activated_value)
@@ -170,10 +205,13 @@ class TestPhotoModule(unittest.TestCase):
         self.camera_photo.set_hflip(False)
         deactivated_value = self.camera_photo.hflip
 
+        # ----------------------------------------------------------------------------------------#
+        #                                     CHECKS                                              #
+        # ----------------------------------------------------------------------------------------#
+
         self.assertFalse(default_value)
         self.assertTrue(activated_value)
         self.assertFalse(deactivated_value)
-
 
     ##############################################################################################
 
@@ -192,78 +230,99 @@ class TestPhotoModule(unittest.TestCase):
         exist_image_1 = os.path.isfile(image_1)
         exist_image_2 = os.path.isfile(image_2)
 
-        # Checks
-        self.assertGreater(elapsed_time_1, 1)
-        self.assertGreater(elapsed_time_2, 3)
-        self.assertTrue(exist_image_1)
-        self.assertTrue(exist_image_2)
-
         # Delete test images
         os.remove(image_1)
         os.remove(image_2)
 
         # Check if the images exist in the destination directory
-        exist_image_1 = os.path.isfile(image_1)
-        exist_image_2 = os.path.isfile(image_2)
+        exist_image_1_after_deleting = os.path.isfile(image_1)
+        exist_image_2_after_deleting = os.path.isfile(image_2)
 
-        # Checks
-        self.assertFalse(exist_image_1)
-        self.assertFalse(exist_image_2)
+        # ----------------------------------------------------------------------------------------#
+        #                                     CHECKS                                              #
+        # ----------------------------------------------------------------------------------------#
 
-        ##############################################################################################
+        self.assertGreater(elapsed_time_1, 1)
+        self.assertGreater(elapsed_time_2, 3)
+        self.assertTrue(exist_image_1)
+        self.assertTrue(exist_image_2)
+        self.assertFalse(exist_image_1_after_deleting)
+        self.assertFalse(exist_image_2_after_deleting)
+
+    ##############################################################################################
 
     def test8_capture_sequence(self):
 
         # Capture of 3 photos.
-        test1_basename = self.camera_photo.capture_sequence()
+        capture_images_test_1 = 3
+        test1_basename = self.camera_photo.capture_sequence(capture_images_test_1)
 
         # Check if the images exist in the destination directory
-        exist_image_1 = os.path.isfile(test1_basename + "-1")
-        exist_image_2 = os.path.isfile(test1_basename + "-2")
-        exist_image_3 = os.path.isfile(test1_basename + "-3")
+        sequence_photo_name_test_1 = []
 
-        # Sequence of 15 photos
-        capture_images_example = 5
-        test2_basename = self.camera_photo.capture_sequence(capture_images_example) # 15 photo captures
-        sequence_photo_name = []
+        for i in range(capture_images_test_1):
+            sequence_photo_name_test_1.append(os.path.exists(test1_basename + "-" + repr(i + 1)))
 
-        for i in range(capture_images_example):
-            sequence_photo_name.append(os.path.exists(test2_basename + "-"  + repr(i+1)))
+        # Sequence of 5 photos
+        capture_images_test_2 = 5
+        test2_basename = self.camera_photo.capture_sequence(capture_images_test_2)  # 5 photo captures
+        sequence_photo_name_test_2 = []
 
-        # Checks if photo tests exist
-        self.assertTrue(exist_image_1)
-        self.assertTrue(exist_image_2)
-        self.assertTrue(exist_image_3)
-
-        for i in range(capture_images_example):
-            self.assertTrue(sequence_photo_name[i])
+        for i in range(capture_images_test_2):
+            sequence_photo_name_test_2.append(os.path.exists(test2_basename + "-" + repr(i + 1)))
 
         # Delete test photos
+        for i in range(capture_images_test_1):
+            os.remove(test1_basename + "-" + repr(i + 1))
 
-        os.remove(test1_basename + "-1")
-        os.remove(test1_basename + "-2")
-        os.remove(test1_basename + "-3")
+        for i in range(capture_images_test_2):
+            os.remove(test2_basename + "-" + repr(i + 1))
 
-        for i in range(capture_images_example):
-            os.remove(test2_basename + "-" + repr(i+1))
+        sequence_photo_name_test_1_after_deleting = []
+        sequence_photo_name_test_2_after_deleting = []
 
-        exist_image_1 = os.path.isfile(test1_basename + "-1")
-        exist_image_2 = os.path.isfile(test1_basename + "-2")
-        exist_image_3 = os.path.isfile(test1_basename + "-3")
+        for i in range(capture_images_test_1):
+            sequence_photo_name_test_1_after_deleting.append(os.path.exists(test1_basename + "-" + repr(i + 1)))
 
-        sequence_photo_name.clear()
+        for i in range(capture_images_test_2):
+            sequence_photo_name_test_2_after_deleting.append(os.path.exists(test2_basename + "-" + repr(i + 1)))
 
-        for i in range(capture_images_example):
-            sequence_photo_name.append(os.path.exists(test2_basename + "-"  + repr(i+1)))
+        # ----------------------------------------------------------------------------------------#
+        #                                     CHECKS                                              #
+        # ----------------------------------------------------------------------------------------#
 
-        # Checks if photo tests do not exist
-        self.assertFalse(exist_image_1)
-        self.assertFalse(exist_image_2)
-        self.assertFalse(exist_image_3)
+        # Photo sequence 1 files created
+        for i in range(capture_images_test_1):
+            self.assertTrue(sequence_photo_name_test_1[i])
 
-        for i in range(capture_images_example):
-            self.assertFalse(sequence_photo_name[i])
+        # Photo sequence 2 files created
+        for i in range(capture_images_test_2):
+            self.assertTrue(sequence_photo_name_test_2[i])
 
+        # Photo sequence 1 files deleted
+        for i in range(capture_images_test_1):
+            self.assertFalse(sequence_photo_name_test_1_after_deleting[i])
+
+        # Photo sequence 2 files deleted
+        for i in range(capture_images_test_2):
+            self.assertFalse(sequence_photo_name_test_2_after_deleting[i])
+
+    ##############################################################################################
+
+    def test9_camera_close(self):
+        # Save open status
+        camera_state_1 = self.camera_photo.camera.closed
+
+        # Close camera
+        self.camera_photo.close()
+        camera_state_2 = self.camera_photo.camera.closed
+
+        # ----------------------------------------------------------------------------------------#
+        #                                     CHECKS                                              #
+        # ----------------------------------------------------------------------------------------#
+
+        self.assertFalse(camera_state_1)
+        self.assertTrue(camera_state_2)
 
 ##############################################################################################
 
