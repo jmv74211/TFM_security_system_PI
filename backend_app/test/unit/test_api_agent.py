c0136431f5ea22c050b76e067bbe488e6927969e
jmv74211 <jmv74211@gmail.com>
2019-07-18 21:37:13 +0200

    Added new unit test to api agent module #11

--- backend_app/test/unit/test_api_agent.py
+++ backend_app/test/unit/test_api_agent.py
@@ -9,6 +9,7 @@ import agents.api_agent as api
     API agent unit tests
 """
 
+
 class TestAPIAgentModule(unittest.TestCase):
 
     @classmethod
@@ -18,18 +19,22 @@ class TestAPIAgentModule(unittest.TestCase):
 
     ##############################################################################################
 
-    def test1_read_photo_configuration(self):
+    def setUp(self):
+        celery.conf.update(task_always_eager=True)
 
+    ##############################################################################################
+
+    def test1_read_photo_configuration(self):
         # Sample configuration file
         file_content = b'---\n\nphoto:\n    resolution: "HIGH"\n    rotation: 180\n   ' \
                        b' vflip: False\n    hflip: True\n' \
                        b'\nvideo:\n    resolution: "MEDIUM"\n   ' \
                        b' rotation: 90\n    vflip: False\n    hflip: False\n    showDatetime: True\n\n\n\n\n'
 
-        file_path = os.path.join(self.test_path,"modules_config.yml")
+        file_path = os.path.join(self.test_path, "modules_config.yml")
 
         # Writing content
-        f = open(file_path,'w')
+        f = open(file_path, 'w')
         f.write(file_content.decode("utf-8"))
         f.close()
 
@@ -70,10 +75,10 @@ class TestAPIAgentModule(unittest.TestCase):
                        b'\nvideo:\n    resolution: "MEDIUM"\n   ' \
                        b' rotation: 90\n    vflip: False\n    hflip: False\n    showDatetime: True\n\n\n\n\n'
 
-        file_path = os.path.join(self.test_path,"modules_config.yml")
+        file_path = os.path.join(self.test_path, "modules_config.yml")
 
         # Writing content
-        f = open(file_path,'w')
+        f = open(file_path, 'w')
         f.write(file_content.decode("utf-8"))
         f.close()
 
@@ -108,3 +113,47 @@ class TestAPIAgentModule(unittest.TestCase):
         self.assertFalse(exist_config_file_after_deleting)
 
     ##############################################################################################
+
+    def test3_take_photo(self):
+        # Take photo
+        photo_file = take_photo(photo_path=self.test_path)
+
+        # Check if the image exist in the destination directory
+        exist_image = os.path.isfile(photo_file)
+
+        # Delete test image
+        os.remove(photo_file)
+
+        # Check if the image exist in the destination directory
+        exist_image_after_deleting = os.path.isfile(photo_file)
+
+        # ----------------------------------------------------------------------------------------#
+        #                                     CHECKS                                              #
+        # ----------------------------------------------------------------------------------------#
+
+        self.assertTrue(exist_image)
+        self.assertFalse(exist_image_after_deleting)
+
+    ##############################################################################################
+
+    def test4_record_video(self):
+        # Record a video
+        video_file = record_video(record_time=5, video_path=self.test_path)
+
+        # Check if the video exist in the destination directory
+        exist_video = os.path.isfile(video_file)
+
+        # Delete test video
+        os.remove(video_file)
+
+        # Check if the video exist in the destination directory
+        exist_video_after_deleting = os.path.isfile(video_file)
+
+        # ----------------------------------------------------------------------------------------#
+        #                                     CHECKS                                              #
+        # ----------------------------------------------------------------------------------------#
+
+        self.assertTrue(exist_video)
+        self.assertFalse(exist_video_after_deleting)
+
+        ##############################################################################################
