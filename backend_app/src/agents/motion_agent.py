86bbc385698ded8c17bb435c871cc34ff79ae1b5
jmv74211 <jmv74211@gmail.com>
2019-07-25 22:41:30 +0200

    Added interaction of the motion agent with the object detection agent #13

--- backend_app/src/agents/motion_agent.py
+++ backend_app/src/agents/motion_agent.py
@@ -4,6 +4,7 @@ import requests
 import settings
 import yaml
 import sys
+import time
 from modules.photo import Photo
 from modules.video import Video
 from agents.api_agent import read_photo_configuration, read_video_configuration
@@ -27,6 +28,9 @@ with open(settings.CONFIG_FILE_AUTHENTICATION, 'r') as ymlfile:
 # Main agent host
 main_agent_host = settings.API_AGENT_IP_ADDRESS + ":{}".format(settings.API_AGENT_RUNNING_PORT)
 
+# Detector agent
+detector_agent_host = settings.DETECTOR_AGENT_IP_ADDRESS + ":{}".format(settings.DETECTOR_AGENT_RUNNING_PORT)
+
 user = cfg['authentication']['user']
 password = settings.API_PASSWORD
 
@@ -48,44 +52,80 @@ logger = MotionAgentLogger()
 
 ##############################################################################################
 
-print("Motion agent is running")
+def detect_person(objects):
+    person_detected = False;
+    list_objects = objects['objects']
 
-# If program has a video parameter
-if len(sys.argv) > 1 and sys.argv[1] == "video":
-    motion_agent_mode = "video"
+    for item in list_objects:
+        for key, value in item.items():
+            if key == 'person':
+                person_detected = True
 
-while True:
+    return person_detected
 
-    sleep(activate_time)
-    if GPIO.input(pin_number):
-        logger.info("ALERT FROM MOTION AGENT")
+##############################################################################################
 
-        if motion_agent_mode == "photo":
-            photo_config = read_photo_configuration()
+if __name__ == "__main__":
 
-            camera_photo = Photo(file_path=settings.ALERTS_FILES_PATH, resolution=photo_config['resolution'],
-                                 vflip=photo_config['vflip'], hflip=photo_config['hflip'])
-            camera_photo.rotate(photo_config['rotation'])
-            file_path = camera_photo.take_photo()
-            camera_photo.close()
-        else:
-            video_config = read_video_configuration()
+    print("Motion agent is running")
 
-            video_camera = Video(file_path=settings.ALERTS_FILES_PATH, showDatetime=video_config['showDatetime'],
-                                 resolution=video_config['resolution'], vflip=video_config['vflip'],
-                                 hflip=video_config['hflip'])
-            video_camera.rotate(video_config['rotation'])
+    # If program has a video parameter
+    if len(sys.argv) > 1 and sys.argv[1] == "video":
+        motion_agent_mode = "video"
 
-            file_path = video_camera.record_video(record_time=15)
-            video_camera.close()
+    while True:
 
-        payload = {'user': user, 'password': password, 'file_path': file_path}
-        headers = {'content-type': 'application/json'}
+        sleep(activate_time)
+        if GPIO.input(pin_number):
+            logger.info("ALERT FROM MOTION AGENT")
 
-        try:
-            motion_agent_request = requests.post(main_agent_host + "/api/motion_agent/generate_alert", json=payload,
-                                                 headers=headers)
-        except:
-            logger.error("Error while trying to send an alert to API agent with address {}.¿It is running?".format(
-                main_agent_host))
-        sleep(settings.REFRESH_TIME)
+            if motion_agent_mode == "photo":
+                photo_config = read_photo_configuration()
+
+                camera_photo = Photo(file_path=settings.ALERTS_FILES_PATH, resolution=photo_config['resolution'],
+                                     vflip=photo_config['vflip'], hflip=photo_config['hflip'])
+                camera_photo.rotate(photo_config['rotation'])
+                file_path = camera_photo.take_photo()
+                camera_photo.close()
+            else:
+                video_config = read_video_configuration()
+
+                video_camera = Video(file_path=settings.ALERTS_FILES_PATH, showDatetime=video_config['showDatetime'],
+                                     resolution=video_config['resolution'], vflip=video_config['vflip'],
+                                     hflip=video_config['hflip'])
+                video_camera.rotate(video_config['rotation'])
+
+                file_path = video_camera.record_video(record_time=15)
+                video_camera.close()
+
+            payload = {'user': user, 'password': password, 'file_path': file_path}
+            headers = {'content-type': 'application/json'}
+
+            # If detector agent is enabled, filter the image and generate an alert if there is a person in the photo
+            if settings.DETECTOR_AGENT_STATUS:
+
+                try:
+                    logger.debug("Sending a request to detector agent")
+                    start_time = time.time()
+                    req = requests.get(detector_agent_host + "/api/detector", json=payload, headers=headers)
+                    detector_time = time.time() - start_time
+                    logger.info("Detector request sent, and it has been responsed in = {} seconds".format(detector_time))
+                except:
+                    logger.error(
+                        "Error while trying to send an alert to Detector API agent with address {}.¿It is running?".format(
+                            detector_agent_host))
+
+                person_detected = detect_person(req.json())
+
+                logger.info("Person detected = {}".format(person_detected))
+            # If detector agent is disabled, generate an alert
+            else:
+                try:
+                    motion_agent_request = requests.post(main_agent_host + "/api/motion_agent/generate_alert", json=payload,
+                                                         headers=headers)
+                except:
+                    logger.error("Error while trying to send an alert to API agent with address {}.¿It is running?".format(
+                        main_agent_host))
+
+            sleep(settings.REFRESH_TIME)
+
