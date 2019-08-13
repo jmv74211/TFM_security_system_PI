b59003cf482f1a19852f53a1332c1d1e065c6f73
jmv74211 <jmv74211@gmail.com>
2019-08-13 19:12:47 +0200

    Updated to generic credentials in authentication unit test close #12

--- test/unit/test_authentication.py
+++ test/unit/test_authentication.py
@@ -1,6 +1,7 @@
 from modules.authentication import authenticate_user
 import unittest
-import os
+import settings
+import yaml
 import getpass  # Get username like whoami
 
 
@@ -15,7 +16,10 @@ class TestAPIAgentModule(unittest.TestCase):
 
     def test1_authenticate_user(self):
 
-        check_1 = authenticate_user("user", "test") # Correct
+        with open(settings.CONFIG_FILE_AUTHENTICATION, 'r') as ymlfile:
+            cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
+        check_1 = authenticate_user(cfg['authentication']['user'], settings.API_PASSWORD) # Correct
         check_2 = authenticate_user("aasdas34f", "test")
         check_3 = authenticate_user("userTest", "hest" )
         check_4 = authenticate_user("usa12", "hdas2")
